1b762b1ee304cddca5de22896855590e4f40ae8a
octogene <ooctogene@gmail.com>
2019-10-14 09:18:43 +0200

    Replace vue-socket.io with vue-socket.io-extended

--- src/main.ts
+++ src/main.ts
@@ -1,7 +1,12 @@
 import Vue from 'vue';
 import App from './App.vue';
+import VueSocketIOExt from 'vue-socket.io-extended';
+import io from 'socket.io-client';
+
+const socket = io.connect("http://localhost:3000");
 
 Vue.config.productionTip = false;
+Vue.use(VueSocketIOExt, socket);
 
 new Vue({
   render: (h) => h(App),
