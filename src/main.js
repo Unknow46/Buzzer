efa56671a555002a353f625419c8e29c588518d7
octogene <ooctogene@gmail.com>
2019-10-22 16:32:09 +0200

    Add Bootstrap to project

--- src/main.js
+++ src/main.js
@@ -1,8 +1,12 @@
 import Vue from 'vue';
 import App from './App.vue';
 import VueSocketIO from 'vue-socket.io';
+import BootstrapVue from 'bootstrap-vue';
+import 'bootstrap/dist/css/bootstrap.css'
+import 'bootstrap-vue/dist/bootstrap-vue.css'
 
 Vue.config.productionTip = false;
+Vue.use(BootstrapVue);
 Vue.use(new VueSocketIO({
   debug: true,
   connection: 'http://localhost:3000'
