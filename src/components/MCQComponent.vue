bd69cdef53b90eb3fba4308f0529ed748ad47112
Raphael <raphaelbauvin@yahoo.fr>
2019-10-22 09:31:16 +0200

    creation components

---
+++ src/components/MCQComponent.vue
@@ -0,0 +1,57 @@
+<template>
+  <div id="MCQ">
+    <h1>{{ question }}</h1>
+    <ul>
+      <li>A - {{ reponse1 }}</li>
+      <li>B - {{ reponse2 }}</li>
+      <li>C - {{ reponse3 }}</li>
+      <li>D - {{ reponse4 }}</li>
+      
+    </ul>
+  </div>
+</template>
+
+<script lang="js">
+
+export default {
+  name: 'MCQ',
+  data: function(){
+    return {
+    question :'Question ...',
+    reponse1 : 'Reponse',
+    reponse2 : 'Reponse',
+    reponse3 : 'Reponse',
+    reponse4 : 'Reponse'
+    }
+  },
+  methods:{
+    updateMCQ:function(){
+      //console.log(this.$socket.client.emit('newGame'));
+      //console.log("yes");
+      //socket.emit(NEXT_QUESTION)
+      //question = 
+      //reponse1 = 
+      //reponse2 = 
+      //reponse3 = 
+      //reponse4  
+    }
+  }
+}
+</script>
+
+<!-- Add "scoped" attribute to limit CSS to this component only -->
+<style scoped>
+ul{
+  list-style: none;
+}
+li {
+  margin: 10px 60px;
+  font-size: 20px;
+  color: white;
+  background-color: #24292e;
+  padding: 20px;
+}
+a {
+  color: #42b983;
+}
+</style>
