1b6fa4499ec4ef404ec3eed34f5678c7f06821ec
octogene <ooctogene@gmail.com>
2019-10-22 09:01:35 +0200

    Fix App.vue imports

--- src/App.vue
+++ src/App.vue
@@ -5,12 +5,17 @@
   </div>
 </template>
 
+<script>
+  import HelloWorld from './components/HelloWorld.vue'
+
 export default {
   name: 'app',
   components: {
     HelloWorld
   }
 }
+
+</script>
 
 <style>
 #app {
