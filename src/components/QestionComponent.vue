fbd6d2714788a1947b6894b5e52236154a81e785
Octogene <ooctogene@gmail.com>
2019-11-11 13:47:45 +0100

    Add default route to Home component
    - Code format on all components

--- src/components/QestionComponent.vue
+++ src/components/QestionComponent.vue
@@ -1,42 +1,42 @@
 <template>
-  <div id="questions">
-      <div v-if="Mcquestions"> 
-        <MCQ/>
-      </div> 
-      <div v-if="Picture"> 
-        <MCQ/>
-      </div>
-      <div v-if="Audio"> 
-        <MCQ/>
-      </div>  
-  </div>
+    <div id="questions">
+        <div v-if="Mcquestions">
+            <MCQ/>
+        </div>
+        <div v-if="Picture">
+            <MCQ/>
+        </div>
+        <div v-if="Audio">
+            <MCQ/>
+        </div>
+    </div>
 </template>
 
 <script lang="js">
-import MCQ from './MCQComponent.vue';
-  
-export default {
-  name: 'Question',
-  components: {
-    MCQ,
-  },
-  data: function(){
-    return {
-    Mcquestions : true,
-    Picture : false,
-    Audio : false,
+    import MCQ from './MCQComponent.vue';
+
+    export default {
+        name: 'Question',
+        components: {
+            MCQ,
+        },
+        data: function () {
+            return {
+                Mcquestions: true,
+                Picture: false,
+                Audio: false,
+            }
+        }
     }
-  }
-}
 </script>
 
 <!-- Add "scoped" attribute to limit CSS to this component only -->
 <style scoped>
-#questions{
-  width: 70%;
-  color: white;
-  padding-top: 60px;
-  padding-left: 40px;
-  font-size: 20px;
-}
+    #questions {
+        width: 70%;
+        color: white;
+        padding-top: 60px;
+        padding-left: 40px;
+        font-size: 20px;
+    }
 </style>
