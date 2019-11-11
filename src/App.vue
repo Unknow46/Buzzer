633ed919c4a0352b4edf7369f11260524cf8dde9
Octogene <ooctogene@gmail.com>
2019-11-11 12:50:26 +0100

    Add routes with Vue Router
    - Add Home as home page

--- src/App.vue
+++ src/App.vue
@@ -1,47 +1,15 @@
 <template>
   <div id="app">
-<<<<<<< HEAD
-    <div v-if="started">
-    <Question/>
-    </div>
-    <div v-else>
-      <b-button @click="create">Créer la partie</b-button>
-      <b-spinner variant="light" label="Spinning"></b-spinner>
-    </div>
-=======
     <b-nav tabs align="center">
       <b-nav-item to='/test'>Test</b-nav-item>
     </b-nav>
     <!-- route outlet -->
     <!-- component matched by the route will render here -->
     <router-view></router-view>
->>>>>>> Add routes with Vue Router
   </div>
 </template>
 
 <script>
-<<<<<<< HEAD
-import Question from './components/QestionComponent.vue'
-
-export default {
-  name: 'app',
-  components: {
-    Question,
-  },
-  data : function(){
-    return{
-      started : false
-    }
-  },
-  methods: {
-    create:function(){
-      this.$socket.emit('newGame',2,3);
-      this.started = true;
-    }
-  }
-}
-=======
->>>>>>> Add routes with Vue Router
 
 </script>
 
