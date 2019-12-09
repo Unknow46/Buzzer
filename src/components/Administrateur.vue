1a574cc990282b7984bca817d6580cabf861bc1e
Nathan <nathan.sens@hotmail.fr>
2019-12-09 11:04:56 +0100

    update

--- src/components/Administrateur.vue
+++ src/components/Administrateur.vue
@@ -1,5 +1,10 @@
-<template>
-    <div></div>
+<template xmlns="http://www.w3.org/1999/html">
+    <div id="container">
+    <div id="team">
+        <p>Entrer le nombre d'équipe :</p>
+        <input name="number_team" id="team_number" aria-valuetext="4">
+    </div>
+    </div>
 </template>
 
 <script>
