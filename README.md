d4b0d3838f71d441dd4f79755afa9c2fca74ea2d
youssef <tyoussef9379@gmail.com>
2020-01-14 15:22:26 +0100

    projet supp

--- README.md
+++
@@ -1,8 +0,0 @@
-Moteur de jeu buzzeurs 
-Version web et mobile d'un jeu de question à choix multiple
-
-Dans 2 semaines sur icampus :
-
-**Note de clarification**
-**Bachlog**
-**sprint bachlog sprint 1**
--- src/utils.js
+++
@@ -1,6 +0,0 @@
-const logger = require('debug');
-export const error = logger('server:error');
-export const debug = logger('server:log');
-export const log = {d: debug, e: error };
-// eslint-disable-next-line no-console
-debug.log = console.log.bind(console);
