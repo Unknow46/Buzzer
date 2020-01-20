0f0adbc16d3fa3a1106d5f6cfc9ed2b603f04e41
Unknow46 <49709624+Unknow46@users.noreply.github.com>
2020-01-20 14:09:19 +0100

    Update Question.vue

--- src/components/Question.vue
+++ src/components/Question.vue
@@ -1,13 +1,13 @@
 <template>
     <div id="question">
-        <div v-if="Mcquestions">
-            <MCQ/>
+        <div v-if="Mcquestions === true">
+            <MCQ :question="this.question"/>
         </div>
-        <div v-if="Picture">
-            <Image/>
+        <div v-if="Picture === true">
+            <ImageQ :question="this.question"/>
         </div>
-        <div v-if="Audio">
-            <MCQ/>
+        <div v-if="Audio === true">
+            <Audio :question="this.question"/>
         </div>
         <div id="toolbar">
             <b-button squared variant="primary" @click="updateMCQ">Next question</b-button>
@@ -17,19 +17,35 @@
 
 <script lang="js">
     import MCQ from './QuestionMCQ.vue';
-    import Image from './QuestionImage.vue';
-
+    import {log} from '../utils';
+    import ImageQ from './QuestionImage.vue';
     export default {
         name: 'Question',
         components: {
             MCQ,
-            Image
+            ImageQ
         },
         data: function () {
             return {
-                Mcquestions: true,
+                Mcquestions: false,
                 Picture: false,
                 Audio: false,
+                question: { question: "No question yet !"}
+            }
+        },
+        sockets: {
+            connect: function () {
+                log.d('socket connected')
+            },
+            'update:questions': function (data) {
+                this.question = data;
+                log.d(`Received data on Questions ${data}`);
+                this.Mcquestions = false;
+                this.Picture = false;
+                this.Audio = false;
+                if (data.type == "QCM") this.Mcquestions = true;
+                if (data.type == "Image") this.Pictures = true;
+                if (data.type == "Audio") this.Audio = true;
             }
         },
         methods: {
@@ -38,7 +54,6 @@
             }
         }
     }
-
 </script>
 
 <!-- Add "scoped" attribute to limit CSS to this component only -->
@@ -50,7 +65,6 @@
         padding-left: 40px;
         font-size: 20px;
     }
-
     #toolbar {
         padding-top: 2rem;
     }
