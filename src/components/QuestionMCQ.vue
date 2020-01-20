44801a9b2a7c72a6b935d2ec5830a1e2045da03a
Unknow46 <49709624+Unknow46@users.noreply.github.com>
2020-01-20 14:05:54 +0100

    Update

--- src/components/QuestionMCQ.vue
+++ src/components/QuestionMCQ.vue
@@ -1,61 +1,36 @@
 <template>
-<<<<<<< HEAD
-<<<<<<< HEAD
-=======
-  <div id="MCQ">
-    <h1>{{ question }}</h1>
-
-      <b-button disabled class="reponse" block variant="dark"> A -  {{ reponse1 }}</b-button>
-      <b-button disabled class="reponse" block variant="dark"> B -  {{ reponse2 }}</b-button>
-      <b-button disabled class="reponse" block variant="dark"> C -  {{ reponse3 }}</b-button>
-      <b-button disabled class="reponse" block variant="dark"> D -  {{ reponse4 }}</b-button>
-
-  </div>
-=======
->>>>>>> d7fafa8f0f134707cd5d10cf317c768ba626433f
-    <div id="MCQ">
-        <b-modal ref="my-modal" hide-footer>
-            <h3>L'équipe {{teamId}} a buzzé !</h3>
-        </b-modal>
-        <h2>{{ question.question }}</h2>
-        <b-button id="0" class="reponse" block variant="dark" v-on:click="repondre"> A - {{ question.proposition[0] }}
-        </b-button>
-        <b-button id="1" class="reponse" block variant="dark" v-on:click="repondre"> B - {{ question.proposition[1] }}
-        </b-button>
-        <b-button id="2" class="reponse" block variant="dark" v-on:click="repondre"> C - {{ question.proposition[2] }}
-        </b-button>
-        <b-button id="3" class="reponse" block variant="dark" v-on:click="repondre"> D - {{ question.proposition[3] }}
-        </b-button>
+    <div id="question">
+        <div v-if="Mcquestions === true">
+            <MCQ :question="this.question"/>
+        </div>
+        <div v-if="Picture === true">
+            <ImageQ :question="this.question"/>
+        </div>
+        <div v-if="Audio === true">
+            <Audio :question="this.question"/>
+        </div>
+        <div id="toolbar">
+            <b-button squared variant="primary" @click="updateMCQ">Next question</b-button>
+        </div>
     </div>
-<<<<<<< HEAD
-=======
-  <div id="MCQ">
-    <h1>{{ question }}</h1>
-
-      <b-button disabled class="reponse" block variant="dark"> A -  {{ reponse1 }}</b-button>
-      <b-button disabled class="reponse" block variant="dark"> B -  {{ reponse2 }}</b-button>
-      <b-button disabled class="reponse" block variant="dark"> C -  {{ reponse3 }}</b-button>
-      <b-button disabled class="reponse" block variant="dark"> D -  {{ reponse4 }}</b-button>
-
-  </div>
->>>>>>>  init
-=======
->>>>>>> master
->>>>>>> d7fafa8f0f134707cd5d10cf317c768ba626433f
 </template>
 
-<script>
+<script lang="js">
+    import MCQ from './QuestionMCQ.vue';
     import {log} from '../utils';
-
+    import ImageQ from './QuestionImage.vue';
     export default {
-        name: 'MCQ',
+        name: 'Question',
+        components: {
+            MCQ,
+            ImageQ
+        },
         data: function () {
             return {
-                question: 'Question ...',
-                reponse1: 'Reponse',
-                reponse2: 'Reponse',
-                reponse3: 'Reponse',
-                reponse4: 'Reponse'
+                Mcquestions: false,
+                Picture: false,
+                Audio: false,
+                question: { question: "No question yet !"}
             }
         },
         sockets: {
@@ -63,56 +38,34 @@
                 log.d('socket connected')
             },
             'update:questions': function (data) {
-                log.d(`Received data on Questions ${data}`)
-                this.question = data.question;
-                this.reponse1 = data.proposition[0];
-                this.reponse2 = data.proposition[1];
-                this.reponse3 = data.proposition[2];
-                this.reponse4 = data.proposition[3];
-            },
-            'update:state': function (state) {
-                log.d(`Received state ${state}`)
-<<<<<<< HEAD
-<<<<<<< HEAD
-=======
-=======
->>>>>>> d7fafa8f0f134707cd5d10cf317c768ba626433f
-            },
-            'update:answer': function (teamId) {
-                this.teamId = teamId;
-                this.$refs['my-modal'].show();
+                this.question = data;
+                log.d(`Received data on Questions ${data}`);
+                this.Mcquestions = false;
+                this.Picture = false;
+                this.Audio = false;
+                if (data.type == "QCM") this.Mcquestions = true;
+                if (data.type == "Image") this.Pictures = true;
+                if (data.type == "Audio") this.Audio = true;
             }
-
         },
         methods: {
-            repondre: function (event) {
-                console.log(this.question.response)
-                console.log(event.target.id)
-                if (this.question.teamId != -1) {
-                    if (this.question.response == event.target.id) {
-                        this.$socket.emit('validate', this.teamId);
-                        this.$socket.emit('nextQuestion');
-                    } else {
-                        this.$socket.emit('reject', this.teamId);
-                    }
-                    this.teamId = -1;
-                }
-<<<<<<< HEAD
-=======
->>>>>>>  init
-=======
->>>>>>> master
->>>>>>> d7fafa8f0f134707cd5d10cf317c768ba626433f
+            updateMCQ: function () {
+                this.$socket.emit('nextQuestion');
             }
-  }
-}
-
+        }
+    }
 </script>
 
 <!-- Add "scoped" attribute to limit CSS to this component only -->
 <style scoped>
-.reponse {
-  padding: 3%;
-  text-align: justify;
-}
+    #question {
+        width: 70%;
+        color: white;
+        padding-top: 60px;
+        padding-left: 40px;
+        font-size: 20px;
+    }
+    #toolbar {
+        padding-top: 2rem;
+    }
 </style>
